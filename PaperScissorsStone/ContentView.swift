373f0c4c8ba9a91fd38c76c7aeb6d552fdfd70fc
tommy <tommystsai@fetc.net.tw>
2024-04-09 15:06:26 +0800

    init project

---
+++ PaperScissorsStone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaperScissorsStone
+//
+//  Created by Sam Lee iMac24 on 2024/4/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaperScissorsStone/PaperScissorsStoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperScissorsStoneApp.swift
+//  PaperScissorsStone
+//
+//  Created by Sam Lee iMac24 on 2024/4/9.
+//
+
+import SwiftUI
+
+@main
+struct PaperScissorsStoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
